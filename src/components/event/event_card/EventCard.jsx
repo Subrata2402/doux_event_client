be01ca89c739c7764d9987ddd47c18d8a1ecd26e
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2025-02-28 00:42:09 +0530

    merge branch with event-details

--- src/components/event/event_card/EventCard.jsx
+++ src/components/event/event_card/EventCard.jsx
@@ -17,98 +17,11 @@ import { Link } from 'react-router-dom';
  * @returns {JSX.Element} The rendered EventCard component.
  */
 function EventCard(props) {
-  const { profileDetails, isLoggedIn } = useAuth();
+  const { profileDetails } = useAuth();
   const apiConnection = new ApiConnection();
   const [cnfModalShow, setCnfModalShow] = useState(false);
   const [loadingj, setLoadingj] = useState(false);
   const [loadingl, setLoadingl] = useState(false);
-  const [loadingd, setLoadingd] = useState(false);
-
-  /**
-   * Handles the logic for a user to join an event.
-   * 
-   * @async
-   * @function joinEvent
-   * @returns {Promise<void>}
-   * 
-   * @throws Will display a snackbar message if the user is not logged in or if the user is the organizer of the event.
-   * 
-   * @description
-   * - If the user is not logged in, a snackbar message will prompt the user to login.
-   * - If the user is the organizer of the event, a snackbar message will inform the user that they cannot join their own event.
-   * - Sets loading state to true while the request is being processed.
-   * - Sends a request to join the event using the event ID.
-   * - If the request is successful, updates the events list and emits an 'update-event' socket event.
-   * - Displays a snackbar message with the response message from the server.
-   * - Sets loading state to false after the request is processed.
-   */
-  const joinEvent = async () => {
-    if (!isLoggedIn) {
-      return customSnackBar('Please login to join the event');
-    }
-    setLoadingj(true);
-    try {
-      const response = await apiServices.joinEvent(props.event._id);
-      if (response.success) {
-        socket.emit('update-event', response.data);
-      }
-      customSnackBar(response.message);
-    } catch (error) {
-      customSnackBar(error.message);
-    }
-    setLoadingj(false);
-  }
-
-  /**
-   * Asynchronously handles the process of leaving an event.
-   * 
-   * This function sets the loading state to true, attempts to leave the event
-   * using the provided API service, and updates the event list and UI accordingly.
-   * If the operation is successful, it emits an event update via socket and shows
-   * a success message. If an error occurs, it shows an error message.
-   * 
-   * @async
-   * @function leaveEvent
-   * @returns {Promise<void>} A promise that resolves when the operation is complete.
-   */
-  const leaveEvent = async () => {
-    setLoadingl(true);
-    try {
-      const response = await apiServices.leaveEvent(props.event._id);
-      if (response.success) {
-        socket.emit('update-event', response.data);
-      }
-      customSnackBar(response.message);
-    } catch (error) {
-      customSnackBar(error.message);
-    }
-    setLoadingl(false);
-  }
-
-  /**
-   * Deletes an event and updates the event list.
-   *
-   * This function sets the loading state to true, attempts to delete the event using the provided API service,
-   * and updates the event list and notifies other clients via socket if the deletion is successful.
-   * It also displays a custom snackbar message based on the response or error.
-   *
-   * @async
-   * @function deleteEvent
-   * @returns {Promise<void>} A promise that resolves when the event deletion process is complete.
-   */
-  const deleteEvent = async () => {
-    setLoadingd(true);
-    try {
-      const response = await apiServices.deleteEvent(props.event._id);
-      if (response.success) {
-        socket.emit('update-event', response.data);
-      }
-      customSnackBar(response.message);
-    } catch (error) {
-      customSnackBar(error.message);
-    }
-    setLoadingd(false);
-  }
 
   return (
     <div className="event-card">
